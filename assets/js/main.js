bd57bf84c6c3621e76f8751966b24c06975f69a9
stas <kinash.stanislav@surelockkey.com>
2022-06-15 18:41:18 +0100

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Delicious - v4.7.1
-* Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -116,7 +110,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -125,7 +119,7 @@
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
+  on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
@@ -135,7 +129,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -169,7 +163,7 @@
 
   heroCarouselItems.forEach((item, index) => {
     (index === 0) ?
-    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>" :
       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
   });
 
@@ -186,9 +180,9 @@
 
       let menuFilters = select('#menu-flters li', true);
 
-      on('click', '#menu-flters li', function(e) {
+      on('click', '#menu-flters li', function (e) {
         e.preventDefault();
-        menuFilters.forEach(function(el) {
+        menuFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
@@ -244,5 +238,38 @@
       clickable: true
     }
   });
+
+  var trigger = document.querySelector(".book");
+  var rowCheck = document.querySelectorAll(".rowCheck");
+  var memberInfo = document.querySelectorAll(".member-info");
+  var cardItem = document.querySelectorAll(".cardItem");
+  var hrItems = document.querySelectorAll(".hrItems");
+
+  trigger.addEventListener('click', () => {
+
+    if (trigger.innerHTML == 'Компактный вид') {
+      trigger.innerHTML = 'Расширенный вид';
+    } else {
+      trigger.innerHTML = 'Компактный вид';
+    }
+
+    for (const row of rowCheck) {
+      row.classList.toggle('gridRow');
+    }
+
+    for (const member of memberInfo) {
+      member.classList.toggle('off');
+    }
+
+    for (const card of cardItem) {
+      card.classList.toggle('pb-5');
+    }
+
+    for (const hr of hrItems) {
+      hr.classList.toggle('on');
+    }
+
+  });
+
 
 })()
